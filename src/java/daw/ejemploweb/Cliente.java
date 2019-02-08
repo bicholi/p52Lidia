24772941dfec1ff6bb8b6655bb8a8d560f8d2fdc
lidia <lidiacintadog@gmail.com>
2019-02-08 14:16:51 +0100

    c8 clase cliente

---
+++ src/java/daw/ejemploweb/Cliente.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.ejemploweb;
+
+/**
+ *
+ * @author bicho
+ */
+public class Cliente {
+    private String nombre;
+    private String email;
+    private String edad;
+
+    public Cliente(String nombre, String email, String edad) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", email=" + email + ", edad=" + edad + '}';
+    }
+    
+    
+    
+}
